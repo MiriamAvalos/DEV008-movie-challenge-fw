464116b7b25e5253771bdf066bcd969456b3dfc9
Mirisaurio <Mirisaurio>
2023-10-03 19:52:12 -0600

    se añade path, titulo e poster en Movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,20 @@
 import { useLocation } from "react-router-dom";
+import './Movie.css';
 
+  
 export function Movie() {
     const {state} = useLocation()  //useLocation devuelve todas las opciones del componente al que navegamos
     console.log(state)
+    const imageBackdropPath = "https://image.tmdb.org/t/p/w200" + state.backdrop_path
+    const imagePosterPath = "https://image.tmdb.org/t/p/w200"  + state.poster_path
+
   return (
-   <h2>{state.original_title}    </h2>
+    <div className="divComponentMovie">
+   <img className= "imageBackdropPath"  src={imageBackdropPath}/>
+   <img className= "imagePosterPath"  src={imagePosterPath}/>
+   <p className="titleMovieDescription">{state.original_title}    </p>
+   
+   </div>
   );
 }
 
