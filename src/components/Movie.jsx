2c909730edd23a5bb6d519e2cb7ee6437b092aa0
Mirisaurio <Mirisaurio>
2024-02-20 10:50:04 -0600

    se corrige enlace de youtube

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -103,7 +103,7 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to
     {showTrailer && trailerKey && (
       <>
         <div className="youtube-video"> 
-        <iframe src={`http://www.youtube.com/embed/${trailerKey}?rel=0`}
+        <iframe src={`https://www.youtube.com/embed/${trailerKey}?rel=0`}
           width="360" 
           height="780"
           /> </div>
